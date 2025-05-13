bcbe62b2d7eb40ec85f3adf6a447cf9ff1ae7e29
DARKXSIDE78 <nk1010057@gmail.com>
2025-05-13 12:20:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7758844213:AAG2uqRcMxMpJclMlSY5r7
 APP_ID = int(os.environ.get("APP_ID", "28744454"))
 API_HASH = os.environ.get("API_HASH", "debd37cef0ad1a1ce45d0be8e8c3c5e7")
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002275439435"))
+CHANNEL_ID = os.environ.get("CHANNEL_ID", "@BatchBotLog")
 OWNER = os.environ.get("OWNER", "DARKXSIDE78")
 OWNER_ID = int(os.environ.get("OWNER_ID", "7086472788"))
 
